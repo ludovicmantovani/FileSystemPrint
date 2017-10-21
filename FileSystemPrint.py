94b356e0e9284f11efe318e8d2b28fba15599132
ludovic <ludovic.mantov@gmail.com>
2017-10-21 20:24:46 +0000

    Ajout du scan du filesystem et generation xml

--- FileSystemPrint.py
+++ FileSystemPrint.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
+import xml.etree.ElementTree as ET
+import hashlib
 
 def main():
-    pass
+    folderDict= dict()
+    startingPath='.'
+    rt = ET.Element("root")
+    startingPathElement = ET.SubElement(rt,"node", type="folder")
+    startingPathElement.text=startingPath
+    folderDict[startingPath]=startingPathElement
+    for root, dirs, files in os.walk(startingPath, topdown=True):
+        for name in files:
+            folderElement = folderDict[root]
+            newFileElement = ET.SubElement(folderElement, "node", type="file")
+            newFileElement.text=name.decode('utf-8')
+
+        for name in dirs:
+            fullFolderName = os.path.join(root, name)
+            folderElement = folderDict[root]
+            newFolderElement = ET.SubElement(folderElement, "node", type="Folder")
+            newFolderElement.text=name.decode('utf-8')
+            folderDict[fullFolderName] = newFolderElement
+
+    tree = ET.ElementTree(rt)
+    tree.write("filename.xml", encoding="UTF-8", xml_declaration=True)
 
 if __name__ == "__main__":
     main()
