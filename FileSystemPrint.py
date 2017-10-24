2f5f3686049d8bf9b125b1069fb2a95edc81c9f5
ludovic <ludovic.mantov@gmail.com>
2017-10-24 21:15:18 +0000

    Ajout fichier de configuration

--- FileSystemPrint.py
+++ FileSystemPrint.py
@@ -3,11 +3,26 @@
 
 import os
 import xml.etree.ElementTree as ET
-import hashlib
+
+def readConfig(inputFile):
+    global nom_fichier
+    nom_fichier = ''
+    global starting_path
+    starting_path = ''
+    with open(inputFile,"r") as lines:
+        for line in lines:
+            if line.startswith('NOM_FICHIER'):
+                nom_fichier=line.split('=')[1].strip()
+            if line.startswith('STARTING_PATH'):
+                starting_path=line.split('=')[1].strip()
 
 def main():
+    readConfig("configFSP")
     folderDict= dict()
-    startingPath='.'
+    if starting_path == '':
+        startingPath='.'
+    else:
+        startingPath = starting_path
     rt = ET.Element("root")
     startingPathElement = ET.SubElement(rt,"node", type="folder")
     startingPathElement.text=startingPath
@@ -24,9 +39,12 @@ def main():
             newFolderElement = ET.SubElement(folderElement, "node", type="Folder")
             newFolderElement.text=name.decode('utf-8')
             folderDict[fullFolderName] = newFolderElement
-
+    if nom_fichier == '':
+        filename='out'
+    else:
+        filename=nom_fichier
     tree = ET.ElementTree(rt)
-    tree.write("filename.xml", encoding="UTF-8", xml_declaration=True)
+    tree.write(filename+".xml", encoding="UTF-8", xml_declaration=True)
 
 if __name__ == "__main__":
     main()
